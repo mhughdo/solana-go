55d5fba4013c3fce342d182fb8155e1b4e4cca2f
Hugh Do <mhughdo@gmail.com>
2024-07-08 00:33:51 +0900

    feat: reduce stream and err buffers

--- rpc/ws/subscription.go
+++ rpc/ws/subscription.go
@@ -41,8 +41,8 @@ func newSubscription(
 	return &Subscription{
 		req:               req,
 		subID:             0,
-		stream:            make(chan result, 200_000),
-		err:               make(chan error, 100_000),
+		stream:            make(chan result, 2000),
+		err:               make(chan error, 100),
 		closeFunc:         closeFunc,
 		unsubscribeMethod: unsubscribeMethod,
 		decoderFunc:       decoderFunc,
